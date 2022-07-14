e2c389727c150a2445eb2f7ec9fb7f53aebe9a32
gaoxinfu <gaoxinfu@aliyun.com>
2022-07-15 07:54:25 +0800

    init

--- springboot-sample-ztree/src/main/java/com/gaoxinfu/springboot/sample/ztree/controller/IndexController.java
+++ springboot-sample-ztree/src/main/java/com/gaoxinfu/springboot/sample/ztree/controller/IndexController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/index")
-    public String home(Map<String, Object> model) {
+    public String index(Map<String, Object> model) {
         model.put("message", "这里是Index主页");
         return "index";
     }
