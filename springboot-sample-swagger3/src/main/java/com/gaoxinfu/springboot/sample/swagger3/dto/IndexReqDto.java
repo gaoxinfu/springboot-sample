032106606c46ca25fccae0620a28fdbb5051855a
gaoxinfu <gaoxinfu@aliyun.com>
2022-07-17 21:39:57 +0800

    index

--- springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/dto/IndexReqDto.java
+++ springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/dto/IndexReqDto.java
@@ -1,5 +1,7 @@
 package com.gaoxinfu.springboot.sample.swagger3.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,12 @@ import lombok.ToString;
  * @Date:2022/7/17 20:37
  * @Version 1.0.0
  */
+@ApiModel(value = "index入参请求对象")
 @Data
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class IndexReqDto {
+    @ApiModelProperty(value = "id主键")
     private int id;
 }
--- springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/dto/IndexResDto.java
+++ springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/dto/IndexResDto.java
@@ -1,5 +1,7 @@
 package com.gaoxinfu.springboot.sample.swagger3.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +13,18 @@ import lombok.ToString;
  * @Date:2022/7/17 20:37
  * @Version 1.0.0
  */
+@ApiModel(value = "index响应出参对象")
 @Data
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class IndexResDto {
+    @ApiModelProperty(value = "id主键")
     private int id;
+    @ApiModelProperty(value = "姓名")
     private String name;
+    @ApiModelProperty(value = "性别")
     private String sex;
+    @ApiModelProperty(value = "年龄")
     private int age;
 }
