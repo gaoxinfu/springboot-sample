ad060152632d90e049d980db395d5f99e6eeab94
gaoxinfu <gaoxinfu@aliyun.com>
2022-07-17 20:44:50 +0800

    springboot-sample-swagger3

--- springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/controller/IndexController.java
+++ springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.gaoxinfu.springboot.sample.swagger3.controller;
 
-import java.util.Map;
-
+import com.gaoxinfu.springboot.sample.swagger3.dto.IndexReqDto;
+import com.gaoxinfu.springboot.sample.swagger3.dto.IndexResDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date:2022/7/6 21:47
  * @Version 1.0.0
  */
+@Slf4j
 @RestController
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
-    public String index() {
-        return "index";
+    @PostMapping("/index")
+    public IndexResDto index(@RequestBody IndexReqDto indexReqDto) {
+        log.info("indexReqDto = "+indexReqDto);
+        return new IndexResDto(1,"gaoxinfu","man",18);
     }
 }
---
+++ springboot-sample-swagger3/src/main/java/com/gaoxinfu/springboot/sample/swagger3/dto/IndexReqDto.java
@@ -0,0 +1,20 @@
+package com.gaoxinfu.springboot.sample.swagger3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @Description:[一句话描述该类的功能]
+ * @Author:gaoxinfu
+ * @Date:2022/7/17 20:37
+ * @Version 1.0.0
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class IndexReqDto {
+    private int id;
+}
