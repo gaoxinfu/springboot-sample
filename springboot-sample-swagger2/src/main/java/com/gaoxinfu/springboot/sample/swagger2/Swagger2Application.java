45765bb3d15d342fa29aae11e3fdf65fc619c8d7
gaoxinfu <gaoxinfu@aliyun.com>
2022-07-18 09:20:47 +0800

    Update Swagger2Application.java

--- springboot-sample-swagger2/src/main/java/com/gaoxinfu/springboot/sample/swagger2/Swagger2Application.java
+++ springboot-sample-swagger2/src/main/java/com/gaoxinfu/springboot/sample/swagger2/Swagger2Application.java
@@ -3,13 +3,11 @@ package com.gaoxinfu.springboot.sample.swagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.UnknownHostException;
-
 
 @SpringBootApplication
 public class Swagger2Application {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
         SpringApplication.run(Swagger2Application.class, args);
     }
 }
